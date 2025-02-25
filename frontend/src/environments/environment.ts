c2b672e51b5f645f3a0b9a691cd4ae68a08882a7
shekela <lukashekelashvili@gmail.com>
2025-02-25 14:21:54 +0100

    change api url

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:7211',
-    //apiUrl: 'https://gio-portfolio-bwh0dga6b6hga0hz.southeastasia-01.azurewebsites.net',
+    apiUrl: 'https://gio-portfolio-bwh0dga6b6hga0hz.southeastasia-01.azurewebsites.net',
 };
